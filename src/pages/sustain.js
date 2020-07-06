12f4df70da8c6178fb65beee9b79b699f78679b2
felixkwan <felixkwan101@gmail.com>
2020-07-06 19:15:22 +0800

    css update

--- src/pages/sustain.js
+++ src/pages/sustain.js
@@ -18,8 +18,23 @@ const Sustainability = () => (
     </div>
 
     <div className="sustain-wrapper pages-wrapper container">
-      <div className="tab-wrapper">
 
+      <div className="container block-title">
+        <h2 className="section-title sustain-footer-title">Combine 3 Elements</h2>
+        <div className="paragraph section-text">
+            <article>
+              <p>
+                たくさんのこだわりと人の手をかけた我が家のアスパラガス。
+                香り高いアスパラガスはシャキシャキと歯ざわりがよく、噛むほどに甘みが広がります。
+                一本そのまま茹でるだけでも美味しくお召し上がりいただける自信作です。ぜひご賞味ください。
+                ひとつひとつ人の手で選別し、丁寧に包装して皆さんの食卓にお届けします。
+              </p>
+            </article>
+        </div>
+        <div className="divider"></div>
+      </div>
+
+      <div className="tab-wrapper">
         <div className="tab-content">
           <div label="People" className="container">
             <div className="container">
@@ -101,43 +116,15 @@ const Sustainability = () => (
             </div>
           </div>
 
-
-      </div>
-
-      <div className="container block-title">
-        <h2 className="section-title sustain-footer-title">Combine 3 Elements</h2>
-        <div className="paragraph section-text">
-            <article>
-              <p>
-              たくさんのこだわりと人の手をかけた我が家のアスパラガス。
-香り高いアスパラガスはシャキシャキと歯ざわりがよく、噛むほどに甘みが広がります。
-一本そのまま茹でるだけでも美味しくお召し上がりいただける自信作です。ぜひご賞味ください。
-ひとつひとつ人の手で選別し、丁寧に包装して皆さんの食卓にお届けします。
-              </p>
-            </article>
-        </div>
-        <div className="divider"></div>
           <div className="container sustain-footer">
-            <div className="columns is-gapless">
-              <div className="column">
-                  <div className="sustain-img sustain-img-1"></div>
-              </div>
-              <div className="column">
-                  <div className="sustain-img sustain-img-2"></div>
-              </div>
-              <div className="column">
-                  <div className="sustain-img sustain-img-3"></div>
-              </div>
-            </div>
-
             <div className="paragraph section-text">
               <div className="container">
                 <article>
                   <p>
                   たくさんのこだわりと人の手をかけた我が家のアスパラガス。
-    香り高いアスパラガスはシャキシャキと歯ざわりがよく、噛むほどに甘みが広がります。
-    一本そのまま茹でるだけでも美味しくお召し上がりいただける自信作です。ぜひご賞味ください。
-    ひとつひとつ人の手で選別し、丁寧に包装して皆さんの食卓にお届けします。
+                  香り高いアスパラガスはシャキシャキと歯ざわりがよく、噛むほどに甘みが広がります。
+                  一本そのまま茹でるだけでも美味しくお召し上がりいただける自信作です。ぜひご賞味ください。
+                  ひとつひとつ人の手で選別し、丁寧に包装して皆さんの食卓にお届けします。
                   </p>
                 </article>
               </div>
