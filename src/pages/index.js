b72d9898f2aeb40800fff0c69fe7b2f5cbf4300b
felixkwan <felixkwan101@gmail.com>
2020-10-19 17:29:09 +0800

    rebuild home

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,12 @@ const IndexPage = ({intl}) => (
               <div className="hero-body">
                 <div className="container">
                   <div className="columns is-gapless is-desktop">
+                    <div className="column is-half is-hidden-tablet">
+                      <BackgroundImage
+                        fluid={imageData}
+                        className="index-header clearfix">
+                      </BackgroundImage>
+                    </div>
                     <div className="column is-half">
                       <h2 className="heading-chin">
                         為你提供專業、全面、高品質園藝服務。
@@ -51,11 +57,11 @@ const IndexPage = ({intl}) => (
                         Professional,Wide Range, High Quality Services.
                       </h1>
                     </div>
-                    <div className="column is-half">
-                    <BackgroundImage
-                      fluid={imageData}
-                      className="index-header clearfix">
-                    </BackgroundImage>
+                    <div className="column is-half is-hidden-mobile">
+                      <BackgroundImage
+                        fluid={imageData}
+                        className="index-header clearfix">
+                      </BackgroundImage>
                     </div>
                   </div>
                 </div>
@@ -63,46 +69,48 @@ const IndexPage = ({intl}) => (
             </div>
           </div>
 
-          <div className="block-service blocks container clearfix">
-            <div  data-sal="fade"
-                  data-sal-duration="250"
-                  data-sal-delay="100"
-                  data-sal-easing="ease"  
-                  className="container block-title">
-              <h4 className="section-subtitle">{intl.formatMessage({ id: "blockService.subtitle" })}</h4>
-              <h2 className="section-title">{intl.formatMessage({ id: "blockService.title" })}</h2>
-              <div className="divider"></div>
+          <div className="block-service blocks">
+            <div className="service-content container">
+              <h5 className="section-subtitle">Four Lines, For Life</h5>
+              <h3 className="section-title">
+                Serve the best, Make you the best.
+              </h3>
+              <h2 className="section-title">
+                我哋為各大物業管理公司,院校,屋苑提供各類專業園藝服務,咨詢及施工。
+              </h2>
             </div>
-    
-            <div className="service-content">
-              <div className="columns is-gapless">
-                <div  data-sal="fade"
-                      data-sal-duration="350"
-                      data-sal-delay="200"
-                      data-sal-easing="ease"
-                      className="service-img column is-half"></div>
-                <div  data-sal="fade"
-                      data-sal-duration="350"
-                      data-sal-delay="300"
-                      data-sal-easing="ease" 
-                      className="column is-half">
-    
-                  <div className="text-wrapper">
-                    <div className="service-title">
-                      <h4>{intl.formatMessage({ id: "blockService.character" })}</h4>
+          </div>
+
+          <div className="block-info blocks">
+            <div className="info-content">
+              <div className="columns is-gapless is-multiline is-desktop">
+                <div className="info-1-img column is-half"></div>
+                <div className="column is-half">
+                  <div className="text-wrapper info-1">
+                    <div className="info-title">
+                      <h4>Shape Safety</h4>
                     </div>
                     <div className="container">
-                      <article>
-                        <p>
-                          {intl.formatMessage({ id: "blockService.article" })}
-                        </p>
-                      </article>
+                      <h1>Maintain every details in your garden.</h1>
+                      <h2>提供安全可靠綠化環境,遠離隱藏危險。</h2>
                     </div>
-                    <Link 
-                    to="/service/" className="primary-button">{intl.formatMessage({ id: "index.btnService" })}</Link>
-                  </div>    
-                
+                  </div>
                 </div>
+
+                <div className="info-2-img column is-half is-hidden-tablet"></div>
+                <div className="column is-half">
+                  <div className="text-wrapper info-1">
+                    <div className="info-title">
+                      <h4>Shape Safety</h4>
+                    </div>
+                    <div className="container">
+                      <h1>Maintain every details in your garden.</h1>
+                      <h2>提供安全可靠綠化環境,遠離隱藏危險。</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="info-2-img column is-half is-hidden-mobile"></div>
+
               </div>
             </div>
     
@@ -110,11 +118,11 @@ const IndexPage = ({intl}) => (
 
           <div className="block-client blocks">
             <div className="container">
-              <h5 className="blk-label">Our Clients</h5>
-              <h3 className="blk-sub-heading">
+              <h5 className="section-subtitle">Our Clients</h5>
+              <h3 className="section-title">
                 Serve the best, Make you the best.
               </h3>
-              <h2 className="blk-heading">
+              <h2 className="section-title">
                 我哋為各大物業管理公司,院校,屋苑提供各類專業園藝服務,咨詢及施工。
               </h2>
               <div className="client-list columns is-multiline is-2 is-desktop">
@@ -135,16 +143,14 @@ const IndexPage = ({intl}) => (
           </div>
 
           <div className="block-message blocks clearfix">    
-            <div className="container message-wrap">
-              <div className="block-heading">
-                <h2>Our Commit</h2>
-              </div>
-              <div  data-sal="slide-up"
-                    data-sal-duration="350"
-                    data-sal-delay="200"
-                    data-sal-easing="ease"
-                    className="container text-wrapper">
-                <h3>信譽第一,品質至上,追求完美,不斷創新,為客戶提供高端的品質服務。</h3>
+            <div className="container">
+              <div className="message-wrap">
+                <div className="msg-heading">
+                  <h2>Our Commit</h2>
+                </div>
+                <div className="message">
+                  <h3>信譽第一,品質至上,追求完美,不斷創新,為客戶提供高品質的服務。</h3>
+                </div>
               </div>
             </div>
     
