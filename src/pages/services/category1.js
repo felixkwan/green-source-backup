5ec162fd9f2be23432f5bd302a073f282e7fe2cb
felixkwan <felixkwan101@gmail.com>
2020-10-20 16:55:02 +0800

    rebuild service

--- src/pages/services/category1.js
+++ src/pages/services/category1.js
@@ -1,7 +1,7 @@
 import React from "react"
-import { useIntl } from "gatsby-plugin-intl"
 
 import Layout from "../../components/layout"
+import Header from "../../components/header"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 
@@ -28,18 +28,22 @@ const Category1 = () => {
       }
     }
   `)
-  const intl = useIntl();
   
   return (
     <Layout>
+      <Header/>
       <div className="container">
         <div className="page-header">
           <div className="hero is-medium">
             <div className="hero-body">
-              <h1 className="page-title">Service</h1>
-              <h3>信譽第一,品質至上,追求完美,不斷創新,為客戶提供高品質的服務。</h3>
+              <h1 className="page-sub-heading">Garden Maintenance</h1>
+              <h3 className="page-heading chin">信譽第一,品質至上,追求完美,不斷創新,為客戶提供高品質的服務。</h3>
+              <p>保養服隊由多名專業及有經驗的人員組成，服務對象涵蓋屋苑，校園，公私營機構，公園</p>
             </div>
           </div>
+        </div>
+        <div className="page-title">
+          <span>Services</span>
         </div>
       </div>
 
